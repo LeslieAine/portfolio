61ed723ce9bcae54b38c420d56c9184253c749fd
LeslieAine <aineleslie@gmail.com>
2025-10-17 11:03:55 +0300

    Update email

--- components/Connect/ConnectData.jsx
+++ components/Connect/ConnectData.jsx
@@ -5,7 +5,7 @@ export const connectData = {
         title3: "Let's Connect",
         description: "I am always open to discussing new projects, ideas, or collaborations. Whether you have a question or need assistance, feel free to reach out!",
         phone: "https://www.linkedin.com/in/leslieaine/",
-        email: "aineleslie@gmail.com",
+        email: "leslie@leslie.bio",
         address: "121 King St, Melbourne VIC 3000",
     }
 };
